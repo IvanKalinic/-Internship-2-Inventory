401033b2fbd910f7c890776c6e151342ecd77e35
Ivan Kalinić <43390832+IvanKalinic@users.noreply.github.com>
2018-12-11 14:48:34 +0100

    Update CommonProperties.cs
    Corrected mistake in class (missing a constructor)

--- CommonProperties.cs
+++ CommonProperties.cs
@@ -8,9 +8,20 @@ namespace Internship_2_Inventory
     {
         public int SerialNumber { get; set; }
         public string Description { get; set; }
-        public int DateOfPurschase { get; set; }
+        public int DateOfPurchase { get; set; }
         public int MonthsOfWarranty { get; set; }
         public int Price { get; set; }
         public string Manufacturer { get; set; }
-    }
+
+        public CommonProperties(int serialNumber, string description, int dateOfPurchase, int monthsOfWarranty,
+            int price, string manufacturer)
+        {
+
+            SerialNumber = serialNumber;
+            Description = description;
+            DateOfPurchase = dateOfPurchase;
+            MonthsOfWarranty = monthsOfWarranty;
+            Price = price;
+            Manufacturer = manufacturer;
+        }
 }
