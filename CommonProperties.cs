1efa351b3523ce986580400bab59099984b5da21
Ivan Kalinić <43390832+IvanKalinic@users.noreply.github.com>
2018-12-12 03:42:11 +0100

    Update CommonProperties.cs

--- CommonProperties.cs
+++ CommonProperties.cs
@@ -10,24 +10,13 @@ namespace Internship_2_Inventory
     {
         public Guid SerialNumber { get; set; }
         public string Description { get; set; }
-<<<<<<< HEAD
         public string DateOfPurchase { get; set; }
         public int MonthsOfWarranty { get; set; }
         public int Price { get; set; }
         
 
-
         public CommonProperties(Guid serialNumber, string description, string dateOfPurchase, int monthsOfWarranty,
             int price)
-=======
-        public int DateOfPurchase { get; set; }
-        public int MonthsOfWarranty { get; set; }
-        public int Price { get; set; }
-        public string Manufacturer { get; set; }
-
-        public CommonProperties(int serialNumber, string description, int dateOfPurchase, int monthsOfWarranty,
-            int price, string manufacturer)
->>>>>>> 401033b2fbd910f7c890776c6e151342ecd77e35
         {
 
             SerialNumber = serialNumber;
@@ -35,14 +24,9 @@ namespace Internship_2_Inventory
             DateOfPurchase = dateOfPurchase;
             MonthsOfWarranty = monthsOfWarranty;
             Price = price;
-<<<<<<< HEAD
            
         }
        
     }
-    
-=======
-            Manufacturer = manufacturer;
-        }
->>>>>>> 401033b2fbd910f7c890776c6e151342ecd77e35
+
 }
